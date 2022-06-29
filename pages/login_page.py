67978f97155f6194afb16eb44065160d45c51a9d
melaniascastlivceva <melaniya2001@mail.com>
2022-06-29 18:37:59 +0300

    Переходы между страницами(работающие)

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import MainPageLocators, LoginPageLocators
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,12 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         #проверка текущей ссылки
-        assert True = self.current_url(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert "login" in self.browser.current_url, "Login link is not presented"
 
     def should_be_login_form(self):
         #проверка, что есть форма входа
-        assert True = self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         #проверка, что есть форма регистрации на странице
-        assert True = self.is_element_present(*MainPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
