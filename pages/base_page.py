15c88e68b9da2776a56081b6172e04797904d926
melaniascastlivceva <melaniya2001@mail.com>
2022-06-29 16:07:47 +0300

    проверка элемента на странице(исправление ошибок)

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -14,3 +15,6 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
